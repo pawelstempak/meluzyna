e558955c32d9cdab97aac8f274b26198b5f934a7
Paweł Stempak <pawelstempak@gmail.com>
2025-04-09 22:57:20 +0200

    start

---
+++ js/tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: "#00397a",
+                secondary: "#2bcc61",
+                dark: "#0c253f",
+                white: "#fff"
+            },
+        },
+    },
+};
